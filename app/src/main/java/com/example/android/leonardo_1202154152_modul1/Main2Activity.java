fd66b77d46c0dc423b8aea9b8f58052a119da33f
leonardoong <leonardo.ong19@gmail.com>
2018-02-04 19:11:11 +0700

    push kedua

--- app/src/main/java/com/example/android/leonardo_1202154152_modul1/Main2Activity.java
+++ app/src/main/java/com/example/android/leonardo_1202154152_modul1/Main2Activity.java
@@ -9,29 +9,29 @@ import android.widget.Toast;
 import org.w3c.dom.Text;
 
 public class Main2Activity extends AppCompatActivity {
-     int duit = 65500;
+     int duit = 65500; //untuk mendeklarasikan uang yang dipunyai
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        Intent intent = getIntent();
-        String menu = intent.getStringExtra(MainActivity.resi);
-        String porsi = intent.getStringExtra(MainActivity.resi2);
-        String tempat = intent.getStringExtra(MainActivity.resi3);
-        String harga = intent.getStringExtra(MainActivity.resi4);
-        int intharga = Integer.parseInt(harga);
-        TextView text1 = (TextView)findViewById(R.id.menu);
-        TextView text2 = (TextView)findViewById(R.id.porsi);
-        TextView text3 = (TextView)findViewById(R.id.tempat);
-        TextView text4 = (TextView) findViewById(R.id.harga);
-        text1.setText(menu);
-        text2.setText(porsi);
-        text3.setText(tempat);
-        text4.setText(harga);
-        if (duit > intharga){
-            Toast.makeText(this, "Disini aja makan malamnya", Toast.LENGTH_LONG).show();
-        }else if (duit < intharga){
-            Toast.makeText(this, "Jangan disini, uang tidak cukup", Toast.LENGTH_LONG).show();
+        Intent intent = getIntent(); // untuk mengambil intent/data yang dikirim dari activity sebelumnya
+        String menu = intent.getStringExtra(MainActivity.resi); // mengambil data string dengan extra resi
+        String porsi = intent.getStringExtra(MainActivity.resi2); // mengambil data string dengan extra resi2
+        String tempat = intent.getStringExtra(MainActivity.resi3); // mengambil data string dengan extra resi3
+        String harga = intent.getStringExtra(MainActivity.resi4); // mengambil data string dengan extra resi4
+        int intharga = Integer.parseInt(harga); //mengubah data String menjadi int agar dapat dioperasikan
+        TextView text1 = (TextView)findViewById(R.id.menu); //pendekerasian textview menu
+        TextView text2 = (TextView)findViewById(R.id.porsi); //pendekerasian textview porsi
+        TextView text3 = (TextView)findViewById(R.id.tempat); //pendekerasian textview tempat
+        TextView text4 = (TextView) findViewById(R.id.harga); //pendekerasian textview harga
+        text1.setText(menu); //set text1 menjadi data menu
+        text2.setText(porsi); //set text2 menjadi data porsi
+        text3.setText(tempat); //set text3 menjadi data tempat
+        text4.setText(harga); //set text4 menjadi data harga
+        if (duit > intharga){ // pengecekan apakah uang dimiliki cukup
+            Toast.makeText(this, "Disini aja makan malamnya", Toast.LENGTH_LONG).show(); //jika iya akan menempilkan toast seperti di text
+        }else if (duit < intharga){ //jika uang yang dimiliki tidak cukup
+            Toast.makeText(this, "Jangan disini, uang tidak cukup", Toast.LENGTH_LONG).show(); //jika tidak maka akan menampilkan toast seperti di text
         }
     }
 }
--- app/src/main/java/com/example/android/leonardo_1202154152_modul1/MainActivity.java
+++ app/src/main/java/com/example/android/leonardo_1202154152_modul1/MainActivity.java
@@ -8,12 +8,12 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String resi = "resi";
-    public static final String resi2 = "resi2";
-    public static final String resi3 = "resi3";
-    public static final String resi4 = "resi4";
-    private EditText menu;
-    private EditText porsi;
+    public static final String resi = "resi"; //pendeklarasian resi
+    public static final String resi2 = "resi2"; //pendeklarasian resi2
+    public static final String resi3 = "resi3"; //pendeklarasian resi3
+    public static final String resi4 = "resi4"; //pendeklarasian resi4
+    private EditText menu; //pendeklarasian edittext menu
+    private EditText porsi; //pendeklatasian edittext porsi
 
 
     @Override
@@ -21,42 +21,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        menu = (EditText) findViewById(R.id.editMenu);
-        porsi = (EditText) findViewById(R.id.editPorsi);
+        menu = (EditText) findViewById(R.id.editMenu); //mendeklarasikan menu dengan edittext dengan id editMenu
+        porsi = (EditText) findViewById(R.id.editPorsi); //mendeklarasikan porsi dengan edittext dengan id editPorsi
 
     }
 
     public void eatbus(View view) {
-        Intent intent = new Intent(this, Main2Activity.class);
-        String menu_makan = menu.getText().toString();
-        String porsi2 = porsi.getText().toString();
-        int prs = Integer.parseInt(porsi2);
-        if (menu_makan.equals("Nasi Uduk")) {
-            int harga = prs * 50000;
-            String total = Integer.toString(harga);
-            intent.putExtra(resi4, total);
+        Intent intent = new Intent(this, Main2Activity.class); // membuat intent untuk memulai aktivitas selanjutnya
+        String menu_makan = menu.getText().toString(); //mengambil data menu dari edittext dan mejadi type data String
+        String porsi2 = porsi.getText().toString(); //mengambil data porsi dari edittext dan mejadi type data String
+        int prs = Integer.parseInt(porsi2); //mengubah data String menjadi int
+        if (menu_makan.equals("Nasi Uduk")) { //pengecekan apakan menu yang dipilih adalah Nasi Uduk
+            int harga = prs * 50000; //jika ya maka harga total harga di eatbus menjadi porsi * 50.000
+            String total = Integer.toString(harga); //mengkonversikan int menjadi string
+            intent.putExtra(resi4, total); //menyimpan data total pada extra resi4
         }
-        String tempat = "EatBus";
-        intent.putExtra(resi, menu_makan);
-        intent.putExtra(resi2, porsi2);
-        intent.putExtra(resi3, tempat);
-        startActivity(intent);
+        String tempat = "EatBus"; //pendeklarasikan nama tempat
+        intent.putExtra(resi, menu_makan); //menyimpan data menu pada extra resi
+        intent.putExtra(resi2, porsi2); //menyimpan data porsi pada extra resi2
+        intent.putExtra(resi3, tempat); //menyimpan data tempat pada extra resi3
+        startActivity(intent); //memulai aktivitas kedua
     }
 
     public void abnormal(View view) {
-        Intent intent2 = new Intent(this, Main2Activity.class);
-        String menu_makan = menu.getText().toString();
-        String porsi3 = porsi.getText().toString();
-        int prs = Integer.parseInt(porsi3);
-        if (menu_makan.equals("Nasi Uduk")) {
-            int harga = prs * 30000;
-            String total = Integer.toString(harga);
-            intent2.putExtra(resi4, total);
+        Intent intent2 = new Intent(this, Main2Activity.class); // membuat intent untuk memulai aktivitas selanjutnya
+        String menu_makan = menu.getText().toString(); //mengambil data menu dari edittext dan mejadi type data String
+        String porsi3 = porsi.getText().toString(); //mengambil data porsi dari edittext dan mejadi type data String
+        int prs = Integer.parseInt(porsi3); //mengubah data String menjadi int
+        if (menu_makan.equals("Nasi Uduk")) { //pengecekan apakan menu yang dipilih adalah Nasi Uduk
+            int harga = prs * 30000; //jika ya maka harga total harga di eatbus menjadi porsi * 30.000
+            String total = Integer.toString(harga); //mengkonversikan int menjadi string
+            intent2.putExtra(resi4, total); //menyimpan data total pada extra resi4
         }
-        String tempat = "Abnormal";
-        intent2.putExtra(resi, menu_makan);
-        intent2.putExtra(resi2, porsi3);
-        intent2.putExtra(resi3, tempat);
-        startActivity(intent2);
+        String tempat = "Abnormal"; //pendeklarasikan nama tempat
+        intent2.putExtra(resi, menu_makan); //menyimpan data menu pada extra resi
+        intent2.putExtra(resi2, porsi3); //menyimpan data porsi pada extra resi2
+        intent2.putExtra(resi3, tempat); //menyimpan data tempat pada extra resi3
+        startActivity(intent2); //memulai aktivitas kedua
     }
 }
